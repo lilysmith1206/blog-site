9953359b603831928bdf8e4a79ef19e716dd9048
Lily Smith <lilysmith.1206@gmail.com>
2024-10-08 21:00:50 -0400

    Add tabs

---
+++ protected/LylinkBackend/wwwroot/js/tabs.js
@@ -0,0 +1,14 @@
+﻿function showTabContent(event, tabId) {
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(content => {
+        content.style.display = 'none';
+    });
+
+    const tabButtons = document.querySelectorAll('.tab-button');
+    tabButtons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(tabId).style.display = 'block';
+    event.currentTarget.classList.add('active');
+}
