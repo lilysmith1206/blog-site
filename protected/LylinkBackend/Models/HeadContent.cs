c161820d2f85f7d8a91fc992e4fa52f8a9e1f200
Lily Smith <lilysmith.1206@gmail.com>
2025-01-04 01:14:20 -0500

    Move HeadContent head tags out of shared element, allowing dropping of model properties

--- protected/LylinkBackend/Models/HeadContent.cs
+++ protected/LylinkBackend/Models/HeadContent.cs
@@ -2,10 +2,6 @@
 {
     public struct HeadContent
     {
-        public bool NeedsTableStyling { get; set; }
-
-        public bool NeedsEditorTooling { get; set; }
-
         public string Keywords { get; set; }
 
         public string Description { get; set; }
