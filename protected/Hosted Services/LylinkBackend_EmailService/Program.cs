8757c560fcd33b51ddfe616420e5bc10bb6ef78d
Lily Smith <lilysmith.1206@gmail.com>
2025-02-23 09:51:54 -0500

    Modify DI setup for email service to inject database services properly

--- protected/Hosted Services/LylinkBackend_EmailService/Program.cs
+++ protected/Hosted Services/LylinkBackend_EmailService/Program.cs
@@ -1,5 +1,8 @@
+using LylinkBackend_DatabaseAccessLayer.Models;
+using LylinkBackend_DatabaseAccessLayer.Services;
 using LylinkBackend_EmailService.Models;
 using LylinkBackend_EmailService.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace LylinkBackend_EmailService
 {
@@ -8,7 +11,15 @@ namespace LylinkBackend_EmailService
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddDbContext<LylinkdbContext>(options =>
+            {
+                options.UseMySql(builder.Configuration.GetConnectionString("MariaDbConnection"), ServerVersion.Parse("11.5.2-mariadb"));
+            });
+
             builder.Services.AddTransient<IEmailService, EmailService>();
+            builder.Services.AddTransient<IVisitAnalyticsRepository, VisitAnalyticsRepository>();
+            
             builder.Services.AddHostedService<EmailServiceWorker>();
 
             builder.Services.Configure<EmailOptions>(
